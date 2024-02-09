2fe5c1444f8678583a7ce76fc94f1b64e9c0e3d4
Lavesh-modi <laveshmodiag@gmail.com>
2024-02-09 13:35:53 +0530

    modal

--- List/my-app/src/App.js
+++ List/my-app/src/App.js
@@ -1,20 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 import { Regularlist } from "./Component/list/Regularlist";
 import { authors } from "./Data/authors";
 import { LargeListItem } from "./Component/author/LargeListItem";
-import { SmallListItem } from "./Component/author/SmallListItem";
+// import { SmallListItem } from "./Component/author/SmallListItem";
 // import LargeListBook from "./Component/book/Longlistbook";
 // import LargeListBook from "./Component/book/Longlistbook";
 import { books } from "./Data/books";
-import sample from "./Component/book/sample";
-import SmalllistBook from "./Component/book/SmalllistBook";
+import { Modal } from "./Component/Modal";
+import LargeListBook from "./Component/book/LonglistBook";
+// import sample from "./Component/book/sample";
+// import SmalllistBook from "./Component/book/SmalllistBook";
 // import LargeListBook from "./Component/book/Longlistbook";
 function App() {
   return (
     <>
-      <h1> hello APP</h1>
-      <Regularlist
+      {/* <Regularlist
         items={authors}
         ItemComponent={LargeListItem}
         sourceName={"author"}
@@ -29,12 +30,22 @@ function App() {
         ItemComponent={sample}
         sourceName={"books"}
       />
+      
 
 <Regularlist
         items={books}
         ItemComponent={SmalllistBook}
         sourceName={"books"}
-      />
+      /> */}
+      <h2>first one </h2>
+
+      <Modal>
+        <LargeListBook books={books[0]}/>
+      </Modal>
+      <h3>Second one </h3>
+      <Modal>
+        <LargeListItem author={authors[0]} />
+      </Modal>
     </>
   );
 }
--- List/my-app/src/Component/author/LargeListItem.jsx
+++ List/my-app/src/Component/author/LargeListItem.jsx
@@ -1,5 +1,6 @@
-export const LargeListItem = ({ author }) => {
+export const LargeListItem = ({ ...author }) => {
   const {name, age, books, country} = author;
+  console.log("in the author")
 
   return (
     <ul>
